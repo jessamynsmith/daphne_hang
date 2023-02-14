84d3f9339bb796a0c10ddea1c17691a64719f8a8
jessamynsmith <geekchick77@gmail.com>
2023-02-13 22:19:03 -0700

    Added morgan logging to node app

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 'use strict';
 
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 app.use(express.json());
 
+app.use(morgan('combined'));
+
 // API Routes
 
 app.get('/api/v1/hang/', async function(request, response) {
+  console.log('this endpoint does not return a response.');
 });
 
 app.set('port', process.env.PORT || 5000);
